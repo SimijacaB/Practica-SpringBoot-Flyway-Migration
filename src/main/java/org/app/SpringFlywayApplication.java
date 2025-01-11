df5cda2df00f928186cb3e05d217f2e3ead60d30
Yesika_Simijaca <yesikasimi@gmail.com>
2025-01-11 12:40:13 -0500

    Creación del proyecto y conexión al contenedor de Mysql en Docker.

--- src/main/java/org/app/SpringFlywayApplication.java
+++ src/main/java/org/app/SpringFlywayApplication.java
@@ -1,13 +1,34 @@
 package org.app;
 
+import lombok.RequiredArgsConstructor;
+import org.app.entity.User;
+import org.app.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class SpringFlywayApplication {
 
+    private final UserRepository userRepository;
+
+    public SpringFlywayApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringFlywayApplication.class, args);
+    }
+
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<User>> findAll(){
+        return ResponseEntity.ok(this.userRepository.findAll());
     }
 
 }
---
+++ src/main/java/org/app/entity/User.java
@@ -0,0 +1,22 @@
+package org.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+}
